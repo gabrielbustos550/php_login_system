77017b49e1df16444ab970a8630141e5c590d37f
Gabe Bustos <bustosfloresgabriel@gmail.com>
2022-04-14 13:04:04 -0600

    Set Up

---
+++ js/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Project 01</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <a href="index.php"><img src="img/logo-white.png" alt="Blogs Logo"></a>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="discover.php">About Us</a></li>
+                    <li><a href="blog.php">Find Blogs</a></li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                    <li><a href="login.php">Log in</a></li>
+                </ul>
+        </div>
+    </nav>
+    
+    <div class="wrapper">
+        <Section class="index-intro">
+            <h2>Some Basic Categories</h2>
+            <p>Here is an important paragraph that explains the purpose of the website</p>
+        </Section>
+
+        <Section class="index-categories">
+            <h2>Some Basic Categories</h2>
+            <div class="index-categories-list">
+                <div>
+                    <h3>Fun Stuff</h3>
+                </div>
+                <div>
+                    <h3>Serious Stuff</h3>
+                </div>
+                <div>
+                    <h3>Exciting Stuff</h3>
+                </div>
+                <div>
+                    <h3>Boring Stuff</h3>
+                </div>
+            </div>
+        </Section>
+    </div>
+    
+</body>
+
+</html>
+<script src="js/script.js"></script>
